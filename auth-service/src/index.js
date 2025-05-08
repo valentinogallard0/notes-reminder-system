305029d861678c5ee88b4e8dbca62781388288a3
valentinogallard0 <146984250+valentinogallard0@users.noreply.github.com>
2025-05-08 16:19:37 -0600

    Create index.js

---
+++ auth-service/src/index.js
@@ -0,0 +1,7 @@
+console.log('Iniciando Auth Service...');
+
+// Simulamos un servidor express basicco
+const port = process.env.PORT || 3001;
+
+console.log(`Auth Service escuchando en el puerto ${port}`);
+console.log('Servicio listo para autenticar usuarios');
